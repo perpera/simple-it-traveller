b6cdf6c1c0a51772384fe6b24a66b4049bff1c00
perpera <poalina@icloud.com>
2025-05-31 18:12:48 +0300

    get started page added

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,20 @@ import IButton from './components/IButton/IButton.vue'
 </script>
 
 <template>
-  <div class="m-5 text-2xl">Hello Vue! <IButton></IButton></div>
+  <main class="flex h-screen">
+    <section class="flex-1 flex justify-center items-center bg-primary">
+      <div class="text-center text-white px-8">
+        <img class="inline mb-6" src="./assets/img/map-pin.svg" alt="map-pin icon" />
+        <h1 class="font-bold text-[38px] mb-7">IT traveler</h1>
+        <p class="leading-6 mb-11">
+          A simple and convenient web application that will let you mark your favourite places, as
+          well as those you would really like to visit. So don't hesitate and try it out yourself.
+        </p>
+        <IButton></IButton>
+      </div>
+    </section>
+    <section class="flex-1">
+      <img class="max-h-full object-cover w-full" src="./assets/img/map.png" alt="preview map" />
+    </section>
+  </main>
 </template>
--- src/components/IButton/IButton.vue
+++ src/components/IButton/IButton.vue
@@ -1,5 +1,5 @@
 <template>
   <button class="bg-[#ffa279] rounded-xl py-[14px] px-10 text-white font-bold -tracking-wider">
-    Click me
+    Get started
   </button>
 </template>
