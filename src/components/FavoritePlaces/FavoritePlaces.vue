1d311f06d393c11ab2fab6df79f10a5967d5f938
perpera <poalina@icloud.com>
2025-06-03 17:58:18 +0300

    favorites list created

---
+++ src/components/FavoritePlaces/FavoritePlaces.vue
@@ -0,0 +1,12 @@
+<script setup>
+import FavoritePlace from '../FavoritePlace/FavoritePlace.vue'
+import IButton from '../IButton/IButton.vue'
+</script>
+
+<template>
+  <div class="px-6">
+    <div class="text-gray mb-4">Added Pins</div>
+    <FavoritePlace :key="n" v-for="n in 4" />
+    <IButton class="w-full mt-10"> Add pin</IButton>
+  </div>
+</template>
