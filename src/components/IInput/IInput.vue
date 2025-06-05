d3d89cdabe225562bb09771300bbedd35696c934
perpera <poalina@icloud.com>
2025-06-05 18:02:49 +0300

    input component created

---
+++ src/components/IInput/IInput.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({ label: String })
+const text = ref('')
+
+const changeValue = (event) => {
+  text.value = event.target.value
+}
+</script>
+
+<template>
+  <div class="w-full text-[#2c2c2c]">
+    <label class="block">
+      <span class="block text-xs px-3 mb-2">{{ props.label }}: {{ text }}</span>
+      <input
+        class="w-full text-sm rounded-[4px] border-[#eaeaea] border-[1px] py-2 px-3 focus:outline-primary"
+        type="text"
+        @input="changeValue"
+      />
+    </label>
+  </div>
+</template>
